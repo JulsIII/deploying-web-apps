b8e73d67e89f4a40c314a3354be02ac8d62dab8e
Your Name <you@example.com>
2021-01-28 19:26:31 -0600

    1 - Setting Up for Vercel

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import theme from "./theme";
+//import "./styles.css";
+import { ThemeProvider } from "styled-components";
+
+
+
+ReactDOM.render(
+<ThemeProvider theme={theme}>
+  <App />
+</ThemeProvider>,
+document.getElementById('root')
+);
